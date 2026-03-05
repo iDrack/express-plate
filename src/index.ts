15526539f4149ade32eb8bb9eae06ffdb119389e
Thomas Dignoire <tdignoire@pm.me>
2026-03-05 16:12:21 +0100

    Disable Swagger documentation in production environment

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ app.use(express.json({ limit: "10kb" }));
 app.use(cookieParser());
 
 //Swagger
-/* if (process.env.NODE_ENV !== "production") */
+if (process.env.NODE_ENV !== "production")
     app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 //Logging
@@ -43,10 +43,10 @@ AppDataSource.initialize()
     .then(() => {
         logger.info("Database connected successfully.");
         app.listen(port, () => {
+            logger.info(
+                `Server running on port ${port} in ${process.env.NODE_ENV} mode`,
+            );
             if (process.env.NODE_ENV !== "production") {
-                logger.info(
-                    `Server running on port ${port} in ${process.env.NODE_ENV} mode`,
-                );
                 logger.info(
                     `Swagger docs available at http://localhost:${port}/api-docs`,
                 );
